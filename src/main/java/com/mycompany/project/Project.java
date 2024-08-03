634ebdb1cbf6d85764185da863033582ab5e9617
Castro <castro.nunes@aluno.senai.br>
2024-08-03 16:58:33 -0300

    second commit

--- src/main/java/com/mycompany/project/Project.java
+++ src/main/java/com/mycompany/project/Project.java
@@ -1,16 +1,27 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.project;
+//@author castr
+import java.util.Scanner;
 
-/**
- *
- * @author castr
- */
 public class Project {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("Digite a 1 nota do aluno:");
+        float nota_um = ler.nextFloat();
+        
+        System.out.println("Digite a 2 nota do aluno:");
+        float nota_dois = ler.nextFloat();
+        
+        System.out.println("Digite a 3 nota do aluno:");
+        float nota_tres = ler.nextFloat();
+        
+        float media = (nota_um + nota_dois + nota_tres)/3;
+        
+        System.out.println("A media das notas do aluno foi:"+media);
+        
+        
     }
 }

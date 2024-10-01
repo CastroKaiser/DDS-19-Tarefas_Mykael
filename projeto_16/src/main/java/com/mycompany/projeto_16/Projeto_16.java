de1ee3db6e874c3916e942556e8311dcd7d3eecc
Castro <168910263+CastroKaiser@users.noreply.github.com>
2024-09-30 21:38:59 -0300

    Add files via upload

---
+++ projeto_16/src/main/java/com/mycompany/projeto_16/Projeto_16.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_16;
+
+/**
+ *
+ * @author c.nunes
+ */
+public class Projeto_16 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
